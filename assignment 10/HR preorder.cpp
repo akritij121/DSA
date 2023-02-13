d11c0435bc28d0263467ebf3b904044ea252df22
Akriti Jaiswal <107640595+akritij121@users.noreply.github.com>
2023-02-14 01:28:03 +0530

    Create HR preorder.cpp

---
+++ assignment 10/HR preorder.cpp
@@ -0,0 +1,8 @@
+void preOrder(Node *root) {
+        if (root == nullptr){
+            return;
+        }
+        cout << root->data << " ";
+        preOrder(root->left);
+        preOrder(root->right);
+    }
